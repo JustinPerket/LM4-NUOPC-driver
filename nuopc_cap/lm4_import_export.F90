84d2e2a9972417e423b5386880e35570e317ba6c
Justin Perket <Justin.Perket@noaa.gov>
2025-06-23 12:11:35 -0400

    minor cleanup

--- nuopc_cap/lm4_import_export.F90
+++ nuopc_cap/lm4_import_export.F90
@@ -502,10 +502,6 @@ contains
       call state_getimport_2d(importState, 'Faxa_swndf', lm4data_1d=lm4_model%atm_forc%flux_sw_down_nir_dif, rc=rc) ! mean surface downward nir diffuse flux
       call state_getimport_2d(importState, 'Faxa_swndr', lm4data_1d=lm4_model%atm_forc%flux_sw_down_nir_dir, rc=rc) ! mean surface downward nir direct flux
 
-      ! call state_getimport_2d(importState, 'Faxa_swdn' , lm4data_1d=lm4_model%atm_forc%flux_sw, rc=rc) ! mean downward SW heat flux
-      ! call state_getimport_2d(importState, 'Faxa_swnet', lm4data_1d=lm4_model%atm_forc%flux_sw, rc=rc) ! mean_net_sw_flx
-      ! call state_getimport_2d(importState, 'Sa_vfrac',  lm4data_1d=lm4_model%atm_forc%vfrac, rc=rc)  ! vegetation fraction
-      ! call state_getimport_2d(importState, 'Sa_zorl',   lm4data_1d=lm4_model%atm_forc%zorl, rc=rc)   ! roughness length
 
       ! only do this on 1st time step
       if (first_call) then
@@ -532,16 +528,6 @@ contains
          call state_getimport_2d(importState, 'Faxa_swndf', lm4data_2d=lm4_model%atm_forc2d%flux_sw_down_nir_dif, rc=rc)
          call state_getimport_2d(importState, 'Faxa_swndr', lm4data_2d=lm4_model%atm_forc2d%flux_sw_down_nir_dir, rc=rc)
       end if
-
-      ! ! call state_getimport_2d(importState, 'Sa_exner'  , cplr2land%, rc=rc)
-      ! ! if (ChkErr(rc,__LINE__,u_FILE_u)) return
-      ! ! call state_getimport_2d(importState, 'Sa_ustar'  , cplr2land%, rc=rc)
-      ! ! if (ChkErr(rc,__LINE__,u_FILE_u)) return
-
-      ! ! call state_getimport_2d(importState, 'vfrac'     , cplr2land%, rc=rc)
-      ! ! if (ChkErr(rc,__LINE__,u_FILE_u)) return
-      ! ! call state_getimport_2d(importState, 'zorl'      , cplr2land%, rc=rc)
-      ! ! if (ChkErr(rc,__LINE__,u_FILE_u)) return
 
       call ESMF_LogWrite(subname//' done', ESMF_LOGMSG_INFO)
 
@@ -738,7 +724,6 @@ contains
       character(len=*),  intent(in)    :: fldname
       real(r8), optional, intent(inout) :: lm4data_1d(:)      ! 1d, Unstructured Grid output
       real(r8), optional, intent(inout) :: lm4data_2d(:,:)    ! 2d, Structured Grid output
-      ! logical,  optional, intent(in)  :: debug_print
       integer,           intent(out)   :: rc
 
       ! local variables
@@ -764,25 +749,8 @@ contains
 
          ! pass structured grid data to unstructured grid
          if (present(lm4data_1d)) then
-            ! ! JP tmp debug
-            ! write(*,*) 'JPsgi fldname ', trim(fldname)
-            ! write(*,*) 'JPsgi fldptr2d size ', size(fldptr2d)
-            ! write(*,*) 'JPsgi fldptr2d shape ', shape(fldptr2d)
-            ! write(*,*) 'JPsgi lnd%ls, lnd%le ', lnd%ls, lnd%le
-            ! write(*,*) 'JPsgi lnd%gs, lnd%ge ', lnd%gs, lnd%ge
-            ! ! end JP tmp debug
             call mpp_pass_sg_to_ug(lnd%ug_domain, fldptr2d, lm4data_1d)
          end if
-
-         ! if (present(debug_print)) then
-         !    if (debug_print) then
-         !       call ESMF_LogWrite(subname//' '//trim(fldname)//' min/max: '// &
-         !                          trim(str(fldptr2d%min))//' '//trim(str(fldptr2d%max)), ESMF_LOGMSG_INFO)
-
-         !    end if
-         ! end if
-
-
       else
          call ESMF_LogWrite(subname//' '//trim(fldname)//' is not in the state!', ESMF_LOGMSG_INFO)
       end if
@@ -805,7 +773,7 @@ contains
 
       ! local variables
       real(r8), pointer :: fldPtr2d(:,:)
-      character(len=*), parameter :: subname='(lnd_export_export:state_setexport_1d)'
+      character(len=*), parameter :: subname='(lnd_export_export:state_setexport_2d)'
       ! ----------------------------------------------
 
 
@@ -823,7 +791,8 @@ contains
          if (present(lm4data_1d)) then
             call mpp_pass_ug_to_sg(lnd%ug_domain, lm4data_1d, fldptr2d)
          end if
-
+      else
+         call ESMF_LogWrite(subname//' '//trim(fldname)//' is not in the state!', ESMF_LOGMSG_INFO)
       end if
 
    end subroutine state_setexport_2d
