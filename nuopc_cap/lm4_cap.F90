a7eeecf3fcf9393b0c36c633f366594f3ba53365
Justin Perket <Justin.Perket@noaa.gov>
2025-08-21 12:09:36 -0400

    fix flux_up call when coupled

--- nuopc_cap/lm4_cap.F90
+++ nuopc_cap/lm4_cap.F90
@@ -544,10 +544,9 @@ contains
       call update_land_model_fast(lm4_model%From_atm,lm4_model%From_lnd)
 
       if (lm4_model%nml%cpl2atm) then  ! if have active 2-way coupling with atm
-         call flux_down_from_atmos(real(sec), lm4_model)
+         call flux_up_to_atmos(lm4_model)
       end if
 
-      call flux_up_to_atmos(lm4_model)
 
 
       call ESMF_ClockGet(dclock,  CurrTime=CurrTime, currSimTime=model_time, rc=rc)
